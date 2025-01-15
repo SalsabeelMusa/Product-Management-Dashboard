82ce61ff0af412354b4ae0b29915509ff380ac75
SalsabeelMusa <salsalbeel.musa16@gmail.com>
2025-01-15 23:28:39 +0300

    add custom modal component

--- Product Management Dashboard/src/App.tsx
+++ Product Management Dashboard/src/App.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "./components/ProductCard";
 import Modal from "./components/Modal";
+import { Button } from "@headlessui/react";
+import "./styles/productList.css";
 
-const App: React.FC = () => {
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <>
-      <input type="button" value={"Add a Product"} />
+      <Button onClick={openModal}>Add a Product</Button>
+
       <ProductList />
-      <Modal />
+      <Modal isOpen={isOpen} closeModal={closeModal} title="Add a new Product">
+        <Button>Add</Button>
+        <Button>Cancel</Button>
+      </Modal>
     </>
   );
 };
